b54aecb158e39588eadebd20887e9f3e0f39d4e9
Hector Hernandez <98498528+arquidev8@users.noreply.github.com>
2023-04-24 14:35:47 -0400

    2

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def propiedades():
+    url = f'https://witei.com/api/v1/houses/'
+    headers = {'Authorization': 'Bearer 7a54f9633cf443d988c0c49e2b77989b'}
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    propiedades_disponibles = [prop for prop in data['results'] if prop['status'] == 'disponible']
+    return render_template('propiedades.html', propiedades=propiedades_disponibles)
 
 
 if __name__ == '__main__':
